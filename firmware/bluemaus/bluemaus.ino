a37a6d56fb1b139a7da799ebe758a69e5d00ecd6
Sean Caulfield <sean@yak.net>
2017-11-23 17:38:01 -0500

    first stabs at some hardware; initial ir and bt firmwares

---
+++ firmware/bluemaus/bluemaus.ino
@@ -0,0 +1,207 @@
+//
+// bluemaus.ino
+//
+// Modified mouse robot toy for my cats. Now with Bluetooth!
+//
+// Author:  Sean Caulfield <sean@yak.net>
+// License: GPLv2.0
+//
+
+#include<Arduino.h>
+#include <nRF5x_BLE_API.h>
+
+#define PIN_MOTOR_A_PH  0
+#define PIN_MOTOR_A_EN  1
+#define PIN_MOTOR_B_PH  2
+#define PIN_MOTOR_B_EN  3
+#define PIN_LED         13
+
+//
+// UUIDs of service and characteristics
+//
+
+static const uint8_t srv_uuid[] = {
+    0x71, 0x3D, 0x00, 0x00, 0x50, 0x3E, 0x4C, 0x75,
+    0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E
+};
+
+static const uint8_t srv_tx_uuid[] = {
+    0x71, 0x3D, 0x00, 0x03, 0x50, 0x3E, 0x4C, 0x75,
+    0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E
+};
+
+static const uint8_t srv_rx_uuid[] = {
+    0x71, 0x3D, 0x00, 0x02, 0x50, 0x3E, 0x4C, 0x75,
+    0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E
+};
+
+static const uint8_t uart_base_uuid_rev[] = {
+    0x1E, 0x94, 0x8D, 0xF1, 0x48, 0x31, 0x94, 0xBA,
+    0x75, 0x4C, 0x3E, 0x50, 0x00, 0x00, 0x3D, 0x71
+};
+
+//
+// Other constants and magic numbers
+//
+
+//#define TXRX_BUF_LEN    20
+static const size_t CHAR_RX_VAL_BUFSZ = 3;
+static const size_t TXRX_BUF_LEN      = 20;
+static const int BLE_TX_POWER         = 4;
+static const int BLE_ADVERT_TIMEOUT   = 0;   // seconds
+static const int BLE_ADVERT_INTERVAL  = 160; // multiples of 0.625ms (wat)
+static const char SHORT_LOCAL_NAME[]  = "TXRX";
+static const char DEVICE_NAME[]       = "BlueMaus";
+
+//
+// Global objects
+//
+
+BLE    ble;
+Ticker tock;
+
+uint8_t tx_value[TXRX_BUF_LEN] = {0};
+uint8_t rx_value[TXRX_BUF_LEN] = {0};
+bool flip = false;
+
+//
+// BLE characteristics
+//
+
+GattCharacteristic char_tx(
+    srv_tx_uuid,
+    tx_value,
+    1,
+    TXRX_BUF_LEN,
+    GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE
+        | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE_WITHOUT_RESPONSE
+);
+
+GattCharacteristic char_recv(
+    srv_rx_uuid,
+    rx_value,
+    1,
+    TXRX_BUF_LEN,
+    GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY
+);
+
+GattCharacteristic *uartChars[] = {&char_tx, &char_recv};
+
+//
+// Unify characteristics into a service
+//
+
+GattService uartService(
+    srv_uuid,
+    uartChars,
+    sizeof(uartChars) / sizeof(GattCharacteristic *)
+);
+
+//
+// Disconnect callback, restarts advertising
+//
+void disconnectionCallBack(const Gap::DisconnectionCallbackParams_t *params) {
+  Serial.println("Disconnected!");
+  Serial.println("Restarting the advertising process");
+  ble.startAdvertising();
+}
+
+//
+// Callback when we receive (?) bytes from the attached client.
+//
+void gattServerWriteCallBack(const GattWriteCallbackParams *Handler) {
+  uint8_t buf[TXRX_BUF_LEN];
+  uint16_t index;
+  uint16_t bytesRead = TXRX_BUF_LEN;
+
+  if (Handler->handle == char_tx.getValueAttribute().getHandle()) {
+    ble.readCharacteristicValue(char_tx.getValueAttribute().getHandle(), buf, &bytesRead);
+    Serial.print("bytesRead: ");
+    Serial.println(bytesRead, HEX);
+    for(index=0; index<bytesRead; index++) {
+      Serial.print(buf[index], HEX);
+      Serial.print(" ");
+    }
+    Serial.println("");
+
+    /* DO DISPATCH HERE */
+
+  }
+}
+
+// Timer callback
+
+void timerCallBack() {
+    uint8_t buf[CHAR_RX_VAL_BUFSZ];
+    uint32_t now = millis();
+
+    // Update the "receive" characteristic with the time
+    buf[0] = (now & 0x00FF0000) >> 16;
+    buf[1] = (now & 0x0000FF00) >> 8;
+    buf[2] = (now & 0x000000FF) >> 0;
+    ble.updateCharacteristicValue(
+        char_recv.getValueAttribute().getHandle(),
+        buf,
+        CHAR_RX_VAL_BUFSZ
+    );
+
+    // Flip our glow stick to say we're still here
+    if (flip) {
+        digitalWrite(PIN_LED, HIGH);
+    } else {
+        digitalWrite(PIN_LED, LOW);
+    }
+    flip = !flip; // flop
+
+}
+
+void setup() {
+
+    // Setup serial connection, hopefully this doesn't stall?
+    Serial.begin(115200);
+
+    // Setup BLE and callbacks
+    ble.init();
+    ble.onDisconnection(disconnectionCallBack);
+    ble.onDataWritten(gattServerWriteCallBack);
+
+    // Setup advertising info
+    ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED);
+    ble.accumulateAdvertisingPayload(
+        GapAdvertisingData::SHORTENED_LOCAL_NAME,
+        (const uint8_t *)SHORT_LOCAL_NAME,
+        sizeof(SHORT_LOCAL_NAME) - 1
+    );
+    ble.accumulateAdvertisingPayload(
+        GapAdvertisingData::COMPLETE_LIST_128BIT_SERVICE_IDS,
+        (const uint8_t *)uart_base_uuid_rev,
+        sizeof(uart_base_uuid_rev)
+    );
+    ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+    ble.setAdvertisingInterval(BLE_ADVERT_INTERVAL);
+    ble.setAdvertisingTimeout(BLE_ADVERT_TIMEOUT);
+
+    // Register UART service
+    ble.addService(uartService);
+
+    ble.setDeviceName((const uint8_t *)DEVICE_NAME);
+    ble.setTxPower(BLE_TX_POWER);
+
+    // Start advertising
+    ble.startAdvertising();
+
+    // Set LED phasers to GLOW (wat)
+    pinMode(PIN_LED, OUTPUT);
+    digitalWrite(PIN_LED, HIGH);
+
+    // Setup timer (every 200ms)
+    //tock.attach_us(timerCallBack, 200000);
+    tock.attach(timerCallBack, 1);
+
+    Serial.println("setup() done");
+
+}
+
+void loop() {
+    ble.waitForEvent();
+}
---
+++ firmware/vibrobot/vibrobot.ino
@@ -0,0 +1,195 @@
+//
+// vibrobot.ino
+//
+// Modified mouse robot toy for my cats.
+//
+// Author:  Sean Caulfield <sean@yak.net>
+// License: GPLv2.0
+//
+
+#include<Arduino.h>
+#include<IRremote.h>
+#include<Adafruit_DotStar.h>
+#include<Metro.h>
+#include "wdt.h"
+
+#define DOTSTAR_DAT 7
+#define DOTSTAR_CLK 8
+
+#define PIN_MOTOR_PH  0
+#define PIN_MOTOR_EN  2
+#define PIN_IR_RECV_A 3
+#define PIN_IR_RECV_B 4
+//#define PIN_LED       13
+
+// For SparkFun's custom IR remote
+#define NEC_IR_CODE_UP     0x10EFA05F
+#define NEC_IR_CODE_DOWN   0x10EF00FF
+#define NEC_IR_CODE_LEFT   0x10EF10EF
+#define NEC_IR_CODE_RIGHT  0x10EF807F
+#define NEC_IR_CODE_CENTER 0x10EF20DF
+#define NEC_IR_CODE_A      0x10EFF807
+#define NEC_IR_CODE_B      0x10EF7887
+#define NEC_IR_CODE_C      0x10EF58A7
+#define NEC_IR_CODE_POWER  0x10EFD827
+#define NEC_IR_REPEAT      0xFFFFFFFF
+
+// How often to poll for IR data
+#define TIMER_IR_CHECK_USEC 200000
+#define TIMER_IR_CHECK_MS   200
+
+// PWM limits
+#define PWM_MIN 0
+#define PWM_MAX 128
+#define PWM_INC 32
+#define PWM_DEC PWM_INC
+
+IRrecv recv_a(PIN_IR_RECV_A);
+IRrecv recv_b(PIN_IR_RECV_B);
+
+decode_results a, b;
+
+Adafruit_DotStar glowy = Adafruit_DotStar(
+  1,           // num pixels
+  DOTSTAR_DAT, // data pin
+  DOTSTAR_CLK, // clock pin
+  DOTSTAR_BRG  // color order
+);
+
+int speed = 0;
+
+//Watchdog dog = Watchdog();
+Metro tock = Metro(200);
+
+//void dump_decode(decode_results *results) {
+//  Serial.print(millis());
+//  Serial.print(F(" SQUEAK type:"));
+//  Serial.print(results->decode_type, DEC);
+//  Serial.print(F(" bits:"));
+//  Serial.print(results->bits, DEC);
+//  Serial.print(F(" hex:"));
+//  Serial.print(results->value, HEX);
+//  if (results->overflow)
+//    Serial.print(F(" Overflow!"));
+//  Serial.println();
+//}
+
+void dispatch_decode(decode_results *results) {
+
+  // Ignore non-NEC IR codes
+  if (results->decode_type != NEC) {
+    return;
+  }
+
+  // Dispatch based on incoming IR command
+  switch (results->value) {
+
+    // Go (slowly)
+    case NEC_IR_CODE_A:
+      speed = 32;
+      break;
+
+    // Go (fast)
+    case NEC_IR_CODE_B:
+      speed = 64;
+      break;
+
+    // Go (really fucking fast)
+    case NEC_IR_CODE_C:
+      speed = 128;
+      break;
+
+    // Increase speed one level
+    case NEC_IR_CODE_UP:
+    case NEC_IR_CODE_RIGHT:
+      speed = constrain(speed + PWM_INC, PWM_MIN, PWM_MAX);
+      break;
+
+    // Decrease speed one level
+    case NEC_IR_CODE_LEFT:
+    case NEC_IR_CODE_DOWN:
+      speed = constrain(speed - PWM_DEC, PWM_MIN, PWM_MAX);
+      break;
+
+    // Stop
+    case NEC_IR_CODE_POWER:
+    case NEC_IR_CODE_CENTER:
+      speed = 0;
+      break;
+
+    // Ignore other codes
+    case NEC_IR_REPEAT:
+    default:
+      Serial.print(F("SQUEAK WAT "));
+      Serial.println(results->value, HEX);
+      break;
+  }
+
+  // Set new speed and LED status
+  if (speed > 0) {
+    analogWrite(PIN_MOTOR_PH, speed);
+    digitalWrite(PIN_LED, HIGH);
+    Serial.print(F("SQUEAK "));
+    Serial.println(speed);
+  } else {
+    analogWrite(PIN_MOTOR_PH, 0);
+    digitalWrite(PIN_LED, LOW);
+    Serial.println(F("SQUEAK STOP"));
+  }
+
+}
+
+// Async routine to check for newly received IR data and dispatch any commands found
+void check_for_ir_data() {
+  if (recv_a.decode(&a)) {
+    recv_a.resume();
+    //dump_decode(&a);
+    dispatch_decode(&a);
+  }
+
+  if (recv_b.decode(&b)) {
+    recv_b.resume();
+    //dump_decode(&b);
+    dispatch_decode(&b);
+  }
+}
+
+void setup() {
+
+  // Set motor direction (whatever)
+  pinMode(PIN_MOTOR_PH, OUTPUT);
+  digitalWrite(PIN_MOTOR_PH, LOW);
+
+  // Set motor speed (0)
+  pinMode(PIN_MOTOR_EN, OUTPUT);
+  digitalWrite(PIN_MOTOR_EN, HIGH);
+
+  // Set LED phasers to GLOW (wat)
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
+
+  // But disable the photon torpedoes (WAT)
+  glowy.begin();
+  glowy.setPixelColor(0, 0);
+  glowy.show();
+
+  // Start IR receivers
+  recv_a.enableIRIn();
+  recv_b.enableIRIn();
+
+  // Setup watchdog for sleeping
+  //dog.enable();
+
+}
+
+void loop() {
+  if (tock.check()) {
+    check_for_ir_data();
+    tock.reset();
+  }
+  //dog.sleep();
+  delay(200);
+  //int downtime = Watchdog.sleep();
+  //Serial.print(F("SQUEAK napped for "));
+  //Serial.println(downtime);
+}

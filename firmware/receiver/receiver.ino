e8b10d2b14657e3397200286423de57c103d630d
Sean Caulfield <sean@yak.net>
2018-01-19 14:22:23 -0500

    added CRC16 checks to remote output

--- firmware/receiver/receiver.ino
+++ firmware/receiver/receiver.ino
@@ -10,6 +10,7 @@
 
 #include<Arduino.h>
 #include<IRLibAll.h>
+#include<util/crc16.h>
 
 #define PIN_IRDA 2
 
@@ -17,6 +18,20 @@ IRdecode  decoder;
 //IRrecvPCI recv(PIN_IRDA);
 IRrecv recv(PIN_IRDA);
 uint16_t buff[RECV_BUF_LENGTH];
+
+bool verify(uint32_t cmd) {
+  uint16_t crc_given, crc_calc;
+  uint8_t x, y;
+
+  y = cmd & 0xFF;
+  x = (cmd >> 8) & 0xFF;
+  crc_given = (cmd >> 16) & 0xFFFF;
+
+  crc_calc = _crc_ccitt_update(crc_calc, x);
+  crc_calc = _crc_ccitt_update(crc_calc, y);
+
+  return (crc_given == crc_calc);
+}
 
 void setup() {
 
@@ -42,7 +57,12 @@ void loop() {
     Serial.print(F(" val "));
     Serial.print(decoder.value, HEX);
     Serial.print(F(" bits "));
-    Serial.println(decoder.bits);
+    Serial.print(decoder.bits);
+    if (verify(decoder.value)) {
+      Serial.println(F(" OK"));
+    } else {
+      Serial.println(F(" BAD"));
+    }
     recv.enableIRIn();
   }
 
--- firmware/remote_redmaus3/remote_redmaus3.ino
+++ firmware/remote_redmaus3/remote_redmaus3.ino
@@ -13,6 +13,7 @@
 #include<Wire.h>
 #include<ArduinoNunchuk.h>
 #include<LowPower.h>
+#include<util/crc16.h>
 
 // Enables debugging information output on Serial.
 // Comment out/undefine to disable debugging.
@@ -35,14 +36,19 @@ const uint32_t SLEEP_TIMEOUT_US = 10000L;
 // Amount of time to power down for
 period_t SLEEP_DURATION = SLEEP_1S;
 
+// Interval between message blats (for when the stick is not zeroed, i.e. a
+// user is asking for movement).
+const uint32_t REPEAT_INTERVAL_US = 200;
+
 // Object(s) for processing IR data
 IRsend ir;
 uint32_t cmd = 0;
-uint8_t bits = 32;
+const uint8_t BITS = 32;
 
 // Timers adnd such
 Metro pollTimer = Metro(DELAY_US);
 Metro sleepTimer = Metro(SLEEP_TIMEOUT_US);
+Metro repeatTimer = Metro(REPEAT_INTERVAL_US);
 
 // Wiichuck controller
 ArduinoNunchuk nunchuk = ArduinoNunchuk();
@@ -58,13 +64,33 @@ int16_t curr_y = 0;
 // Joystick calibration data. Numbers are as reported from my 'chuk with stick
 // zeroed. Adjust to your setup as needed.
 int16_t calibration_x = 127;
-int16_t calibration_y = 127;
+int16_t calibration_y = 128;
 
 // Closely related to the calibration values, but not something that will
 // (ever)change is the midpoint of 8 bit unsigned integers (oh for the love of
 // gods and Turing let that be a safe assumption...)
 const int16_t MIDPOINT_X = 127;
 const int16_t MIDPOINT_Y = 127;
+
+const int16_t MIN_X = 0;
+const int16_t MIN_Y = 0;
+const int16_t MAX_X = 255;
+const int16_t MAX_Y = 255;
+
+//////////////////////////////////////////////////////////////////////
+
+// Construct command buffer
+uint32_t command(uint8_t x, uint8_t y) {
+  uint32_t cmd = 0;
+  uint16_t crc = 0;
+
+  crc = _crc_ccitt_update(crc, x);
+  crc = _crc_ccitt_update(crc, y);
+
+  cmd = (((uint32_t)crc) << 16) | (((uint32_t)x) << 8) | ((uint32_t)y);
+
+  return cmd;
+}
 
 //////////////////////////////////////////////////////////////////////
 
@@ -104,13 +130,19 @@ void loop() {
     curr_x = nunchuk.analogX - calibration_x + MIDPOINT_X;
     curr_y = nunchuk.analogY - calibration_y + MIDPOINT_Y;
 
+    // Bound coordinates as I've managed to get a few out of range values due
+    // to calibration offsets.
+    curr_x = constrain(curr_x, MIN_X, MAX_X);
+    curr_y = constrain(curr_y, MIN_Y, MAX_Y);
+
     // Reset poll timer
     pollTimer.reset();
 
   }
 
   // If updated position, send command(s)
-  if (curr_x != last_x || curr_y != last_y) {
+  if (curr_x != last_x || curr_y != last_y ||
+      ((curr_x != MIDPOINT_X || curr_y != MIDPOINT_Y) && repeatTimer.check())) {
 
 #if FLASH_LED
     // For debugging, fire visible LED and dump to serial
@@ -122,10 +154,10 @@ void loop() {
     last_y = curr_y;
 
     // Construct command buffer
-    cmd = (((uint16_t)curr_x) & 0xFF) << 8 | (((uint16_t)curr_y) & 0xFF);
+    cmd = command(curr_x, curr_y);
 
     // Send command buffer
-    ir.send(NECX, cmd, bits);
+    ir.send(NECX, cmd, BITS);
 
 #if FLASH_LED
     // Disable LED since we're done now
@@ -143,6 +175,7 @@ void loop() {
 #endif
 
     // Reset sleep timer since something wiggled
+    repeatTimer.reset();
     sleepTimer.reset();
 
   } else if (sleepTimer.check()) {

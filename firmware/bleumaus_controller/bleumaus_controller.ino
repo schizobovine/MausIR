2f7f37d248b03daeff73165e6b738c213f4d8b66
Sean Caulfield <sean@yak.net>
2019-09-01 15:44:59 -0400

    further work

--- firmware/bleumaus_controller/bleumaus_controller.ino
+++ firmware/bleumaus_controller/bleumaus_controller.ino
@@ -11,6 +11,10 @@
 #include <Wire.h>
 #include <Adafruit_seesaw.h>
 #include <bluefruit.h>
+#include <Adafruit_LittleFS.h>
+#include <InternalFileSystem.h>
+
+//using namespace Adafruit_LittleFS_Namespace;
 
 #define DEBUG           (true)
 
@@ -27,7 +31,10 @@
 #define BUTTON_LEFT     (1 << 9)
 #define BUTTON_UP       (1 << 10)
 #define BUTTON_SEL      (1 << 14)
+#define CHANNEL_X       (3)
+#define CHANNEL_Y       (2)
 
+//const uint32_t BUTTON_MASK = 0xffffffff;
 const uint32_t BUTTON_MASK = 
     BUTTON_RIGHT
   | BUTTON_DOWN
@@ -39,11 +46,24 @@ const uint32_t BUTTON_MASK =
 #define PIN_SEESAW_IRQ  13
 #define I2C_ADDR_SEESAW 0x49
 
-#define MIN_X_DELTA 3
-#define MIN_Y_DELTA 30
+#define MIN_DELTA 3
+#define HAS_MOVED(z, last_z) (abs((z) - (last_z)) > MIN_DELTA)
 
-#define HAS_X_MOVED (abs((x) - (last_x)) > MIN_X_DELTA)
-#define HAS_Y_MOVED (abs((y) - (last_y)) > MIN_Y_DELTA)
+#define CALIBRATE_FILENAME ("/calibrat.dat")
+#define FILE_O_READ (Adafruit_LittleFS_Namespace::FILE_O_READ)
+#define FILE_O_WRITE (Adafruit_LittleFS_Namespace::FILE_O_WRITE)
+
+#define DPAD(x, width, pad_char) \
+    do { \
+        for (size_t _i=width; _i > 0; _i--) { \
+            if ((_i == 1 && (x) >= 0) || (abs(x) < pow(10, _i-1))) { \
+                DPRINT(pad_char); \
+            } \
+        } \
+    } while (0)
+
+const int16_t MOTOR_SCALE_Y = 512;
+const int16_t MOTOR_SCALE_X = 128;
 
 //
 // Global objects
@@ -51,13 +71,18 @@ const uint32_t BUTTON_MASK =
 
 Adafruit_seesaw joy;
 BLEClientUart bleuart;
+Adafruit_LittleFS_Namespace::File caldata(InternalFS);
 
-uint32_t butts = 0; //store buttons pressed in irq handler
-volatile bool changed = false;
-//int last_x = 0;
-//int last_y = 0;
-//int x = 0;
-//int y = 0;
+uint32_t curr_butts = 0;
+uint32_t last_butts = 0;
+int16_t last_x = 0;
+int16_t last_y = 0;
+int16_t x = 0;
+int16_t y = 0;
+int16_t motor_l = 0;
+int16_t motor_r = 0;
+int16_t calibrate_x = 512;
+int16_t calibrate_y = 512;
 
 /**
  * Invoked when UART receives data.
@@ -110,10 +135,58 @@ void scan_callback(ble_gap_evt_adv_report_t *report) {
 }
 
 /**
- * Invoked when the seesaw controller sends an IRQ.
+ * Read in calibration data from save file on internal FS.
  */
-void joy_irq() {
-  changed = true;
+void read_calibration_data() {
+    if (caldata.open(CALIBRATE_FILENAME, FILE_O_READ)) {
+        caldata.read((uint8_t*)&calibrate_x, sizeof(calibrate_x));
+        caldata.read((uint8_t*)&calibrate_y, sizeof(calibrate_y));
+        caldata.close();
+        DPRINT(F("Reading caliration data: x = "));
+        DPRINT(calibrate_x);
+        DPRINT(F(", y = "));
+        DPRINTLN(calibrate_y);
+    }
+}
+
+/**
+ * Write out calibration data to file on internal FS.
+ */
+void write_calibration_data() {
+    if (caldata.open(CALIBRATE_FILENAME, FILE_O_WRITE)) {
+
+        byte buff[4];
+        buff[0] = (calibrate_x >> 8) && 0xFF;
+        buff[1] = (calibrate_x >> 0) && 0xFF;
+        buff[2] = (calibrate_y >> 8) && 0xFF;
+        buff[3] = (calibrate_y >> 0) && 0xFF;
+
+        DPRINT(F("Writing caliration data: x = "));
+        DPRINT(calibrate_x);
+        DPRINT(F(", y = "));
+        DPRINTLN(calibrate_y);
+
+        DPAD(buff[0], 2, "0"); DPRINT(buff[0], HEX);
+        DPAD(buff[1], 2, "0"); DPRINT(buff[1], HEX);
+        DPAD(buff[2], 2, "0"); DPRINT(buff[2], HEX);
+        DPAD(buff[3], 2, "0"); DPRINT(buff[3], HEX);
+        DPRINTLN();
+
+        //caldata.write((uint8_t*)&calibrate_x, sizeof(calibrate_x));
+        //caldata.write((uint8_t*)&calibrate_y, sizeof(calibrate_y));
+        caldata.write(buff, sizeof(buff));
+        caldata.flush();
+        caldata.close();
+
+    }
+}
+
+/**
+ * Recalibrate joystick assuming current coordinates are the new origin.
+ */
+void calibrate_joystick() {
+    calibrate_x = joy.analogRead(CHANNEL_X);
+    calibrate_y = joy.analogRead(CHANNEL_Y);
 }
 
 void setup() {
@@ -135,6 +208,10 @@ void setup() {
     }
     pinMode(PIN_SEESAW_IRQ, INPUT);
 
+    // Read in calibration data from internal FS
+    InternalFS.begin();
+    read_calibration_data();
+
     // Initialize BLE in Central mode
     Bluefruit.begin(0, 1);
     Bluefruit.setName("BleuMaus Controller");
@@ -151,51 +228,81 @@ void setup() {
     Bluefruit.Scanner.useActiveScan(false);
     Bluefruit.Scanner.start(0);
 
-    // Attach interrupt handler to joystick IRQ line
-    attachInterrupt(PIN_SEESAW_IRQ, joy_irq, CHANGE);
-
-    // Set LED phasers to GLOW (wat)
-    //pinMode(PIN_LED, OUTPUT);
-    //digitalWrite(PIN_LED, HIGH);
-
-    Serial.println("setup() done");
+    DPRINTLN(F("setup() done"));
 
 }
 
 void loop() {
 
-  if (changed) {
-    changed = false;
-    butts = joy.digitalReadBulk(BUTTON_MASK);
-    if (!(butts & BUTTON_RIGHT)) {
-      DPRINTLN(F("RIGHT"));
+    curr_butts = joy.digitalReadBulk(BUTTON_MASK);
+    if (last_butts != curr_butts) {
+        last_butts = curr_butts;
+        DPRINTLN(curr_butts, BIN);
+        if (!(curr_butts & BUTTON_RIGHT)) {
+            DPRINTLN(F("RIGHT"));
+        }
+        if (!(curr_butts & BUTTON_LEFT)) {
+            DPRINTLN(F("LEFT"));
+        }
+        if (!(curr_butts & BUTTON_UP)) {
+            DPRINTLN(F("UP"));
+        }
+        if (!(curr_butts & BUTTON_DOWN)) {
+            DPRINTLN(F("DOWN"));
+        }
+        if (!(curr_butts & BUTTON_SEL)) {
+            DPRINTLN(F("SELECT"));
+            // TODO add joystick calibration / recentering function here
+            calibrate_joystick();
+            write_calibration_data();
+        }
     }
-    if (!(butts & BUTTON_LEFT)) {
-      DPRINTLN(F("LEFT"));
-    }
-    if (!(butts & BUTTON_UP)) {
-      DPRINTLN(F("UP"));
-    }
-    if (!(butts & BUTTON_DOWN)) {
-      DPRINTLN(F("DOWN"));
-    }
-    if (!(butts & BUTTON_SEL)) {
-      DPRINTLN(F("SELECT"));
-    }
-  }
 
-  //x = joy.analogRead(2);
-  //y = joy.analogRead(3);
-  //if (HAS_X_MOVED || HAS_Y_MOVED) {
-  //  DPRINT(F("x: "));
-  //  DPRINT(x);
-  //  DPRINT(F(" y: "));
-  //  DPRINT(y);
-  //  DPRINTLN();
-  //  last_x = x;
-  //  last_y = y;
-  //}
+    // Subtracting 512 to re-center origin at 0,0
+    x = joy.analogRead(CHANNEL_X) - calibrate_x;
+    y = -(joy.analogRead(CHANNEL_Y) - calibrate_y);
+
+    // If we've seen enough of a change, dispatch new movement command
+    if (HAS_MOVED(x, last_x) || HAS_MOVED(y, last_y)) {
+
+        //if (y >= 0) {
+        //    motor_l = (x>=0) ? (MOTOR_MAX) : (MOTOR_MAX + x);
+        //    motor_r = (x>=0) ? (MOTOR_MAX - x) : (MOTOR_MAX);
+        //} else {
+        //    motor_l = (x>=0) ? (MOTOR_MAX - x) : (MOTOR_MAX);
+        //    motor_r = (x>=0) ? (MOTOR_MAX) : (MOTOR_MAX + x);
+        //}
+
+        motor_l  = MOTOR_SCALE_Y * y / MOTOR_SCALE_Y;
+        motor_l += MOTOR_SCALE_X * x / MOTOR_SCALE_X;
+
+        motor_r  = MOTOR_SCALE_Y * y / MOTOR_SCALE_Y;
+        motor_r -= MOTOR_SCALE_X * x / MOTOR_SCALE_X;
+
+        //if (x < 0) {
+        //    motor_l = motor_l + ((x<0) ? x : -x);
+        //    motor_r = motor_r + ((x<0) ? -x : x);
+        //} else {
+        //    motor_l = motor_l + ((x<0) ? x : -x);
+        //    motor_r = motor_r + ((x<0) ? -x : x);
+        //}
+
+        //if (y < 0) {
+        //    motor_l = -motor_l;
+        //    motor_r = -motor_r;
+        //}
+
+        DPRINT(F( "x: ")); DPAD(x,       4, F(" ")); DPRINT(x);
+        DPRINT(F(" y: ")); DPAD(y,       4, F(" ")); DPRINT(y);
+        DPRINT(F(" L: ")); DPAD(motor_l, 4, F(" ")); DPRINT(motor_l);
+        DPRINT(F(" R: ")); DPAD(motor_r, 4, F(" ")); DPRINT(motor_r);
+        DPRINTLN();
+
+        last_x = x;
+        last_y = y;
+
+    }
 
 }
 
-// vi: syntax=arduino
+// vi: syntax=arduino ts=4 sw=4 expandtab smarttab
